9f0f464b5f1e55ebbffec95fe10e782a7b9ca7a8
Antony <antony_fedyaev@mail.ru>
2019-04-30 19:57:30 +0700

    init

---
+++ task3/src/main/java/ru/cft/focusstart/Main.java
@@ -0,0 +1,18 @@
+package ru.cft.focusstart;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+//        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+//
+//        JMenuItem exitItem = new JMenuItem("Exit");
+//        exitItem.addActionListener(e -> System.exit(0));
+
+        frame.setResizable(false);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
